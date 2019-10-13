654e6ce4f3193717edd598c05fd5cac1dccf08b7
clemonCC <631434851@qq.com>
2019-10-13 23:25:41 +0800

    优化为无状态组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
 import store from '@/redux/store'
-import routeList from "@/router/routeList"
-import renderRoute from "@/router/routeGen"
+import Container from "@/containers/container"
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = store.getState();
-        store.subscribe(this.storeChange) //订阅Redux的状态
-    }
-
-    storeChange = () => {
-        this.setState(store.getState())
-    }
-
-    render() {
-        return (
-            <Provider store={store}>
-                <BrowserRouter>
-                    { renderRoute(routeList, this.state.authed) }
-                </BrowserRouter>
-            </Provider>
-        );
-    }
+const App = () => {
+    
+    return (
+        <Provider store={store}>
+            <Container />
+        </Provider>
+    );
 }
 
 export default App;
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,16 +3,26 @@ import action_type from "./actionTypes.js"
 const defaultState = {
     user_name : "none",
     user_passwd : "none",
-    authed : false
+    authed : false,
+    num : 0
+}
+
+const deep_copy = (value) => {
+    return JSON.parse(JSON.stringify(value))
 }
 
 
 export default (state = defaultState, action) => {
-    if (action.type === action_type.set_user_name) {
-        let newState = JSON.parse(JSON.stringify(state))
+    if (action.type === action_type.login) {
+        let newState = deep_copy(state)
         newState.user_name = action.user_name
         newState.user_passwd = action.user_passwd
         newState.authed = action.authed
+        return newState
+    }
+    if (action.type === action_type.add) {
+        let newState = deep_copy(state)
+        newState.num += action.num
         return newState
     }
 
